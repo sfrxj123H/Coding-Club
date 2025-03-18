8dca170e9969aad7733e7e19ffc34609803c50cf
sfrxj123H <sfrxj123H@gmail.com>
2025-03-18 08:58:06 +0700

    Update loader.js

--- Coding Club but sigma/MDE Project/article-loader/loader.js
+++ Coding Club but sigma/MDE Project/article-loader/loader.js
@@ -1,10 +1,10 @@
 export function loader(key, toQuerySelector) {
     if (key == "math-k&g1") {
         let docAdd = ""
-        docAdd += "<a href='../math/K-and-G1/numbers'><h3>Numbers</h3></a>"
-        docAdd += "<a href='../math/K-and-G1/addition-and-subtraction'><h3>Addition and Subtraction</h3></a>"
-        docAdd += "<a href='../math/K-and-G1/before-solving-quizzes'><h3>Before solving quizzes</h3></a>"
-        docAdd += "<a href='../math/K-and-G1/quiz/addition-and-subtraction'><h3>Quiz: Solving basic additions and subtractions" + 
+        docAdd += "<a href='../math/k-and-G1/numbers'><h3>Numbers</h3></a>"
+        docAdd += "<a href='../math/k-and-G1/addition-and-subtraction'><h3>Addition and Subtraction</h3></a>"
+        docAdd += "<a href='../math/k-and-G1/before-solving-quizzes'><h3>Before solving quizzes</h3></a>"
+        docAdd += "<a href='../math/k-and-G1/quiz/addition-and-subtraction'><h3>Quiz: Solving basic additions and subtractions" + 
         (localStorage.getItem("quiz-addition-and-subtraction") == "true" ? " (Completed)" : "") +
         "</h3></a>"
         document.querySelector(toQuerySelector).innerHTML = docAdd
